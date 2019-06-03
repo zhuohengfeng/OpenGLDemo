400cba120ed636628e1ac1a067e40bd7c6e8c0cf
hengfeng.zhuo <hengfeng.zhuo@rokid.com>
2019-06-03 14:39:22 +0800

    [add] 添加FBO, EGL demo入口

--- app/src/main/java/com/ryan/opengldemo/MainActivity.java
+++ app/src/main/java/com/ryan/opengldemo/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 import com.ryan.opengldemo.camera.CameraActivity;
 import com.ryan.opengldemo.cube.CubeActivity;
+import com.ryan.opengldemo.egl.EGLActivity;
+import com.ryan.opengldemo.fbo.FBOActivity;
 import com.ryan.opengldemo.texture.TextureActivity;
 import com.ryan.opengldemo.triangle.TriangleActivity;
 
@@ -33,9 +35,19 @@ public class MainActivity extends AppCompatActivity {
         startRenderActivity(TextureActivity.class);
     }
 
-    // 绘制相机预览
+    // 绘制相机预览数据
     public void onStartCamera(View view) {
         startRenderActivity(CameraActivity.class);
+    }
+
+    // FBO的使用
+    public void onStartFBO(View view) {
+        startRenderActivity(FBOActivity.class);
+    }
+
+    // EGL的使用
+    public void onStartEGL(View view) {
+        startRenderActivity(EGLActivity.class);
     }
 
     private void startRenderActivity(Class<?> toClass) {
@@ -43,5 +55,6 @@ public class MainActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         this.startActivity(intent);
     }
+
 
 }
---
+++ app/src/main/java/com/ryan/opengldemo/fbo/FBOActivity.java
@@ -0,0 +1,16 @@
+package com.ryan.opengldemo.fbo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ryan.opengldemo.R;
+
+public class FBOActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fbo);
+    }
+
+}
